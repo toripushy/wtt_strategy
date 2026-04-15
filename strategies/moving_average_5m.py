5da538cef1dc5def7a8ed6a5aa6d04066c409bc9
toripushy <62052919+toripushy@users.noreply.github.com>
2026-04-15 15:53:27 +0800

    Add sample 5m strategy

---
+++ strategies/moving_average_5m.py
@@ -0,0 +1,17 @@
+class Strategy:
+    frequency = "5m"
+
+    def __init__(self):
+        self.fast = 5
+        self.slow = 20
+
+    def on_bar(self, ctx, data):
+        for symbol in data:
+            fast = ctx.history(symbol, "close", self.fast)
+            slow = ctx.history(symbol, "close", self.slow)
+            if len(slow) < self.slow:
+                continue
+            if sum(fast) / len(fast) > sum(slow) / len(slow):
+                ctx.order_target_percent(symbol, 0.95)
+            else:
+                ctx.order_target_percent(symbol, 0.0)
